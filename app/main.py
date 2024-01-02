bbbc4a96010b7a91a4e1adcf36ede0f8f1f99fa6
ShubhrangiD <shubhrangidixit@gmail.com>
2024-01-02 14:03:36 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
-
 import socket
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
-
+    
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
